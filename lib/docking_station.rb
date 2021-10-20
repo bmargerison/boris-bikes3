9ac6e9afdb74ecb4e3b927127075195ad1d652fa
Ginny <ginnynjon@gmail.com>
2021-10-20 17:39:01 +0100

    worked through step 11, program can now return a bike that has been docked

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,4 +9,16 @@ class DockingStation
         Bike.new 
     end
 
+    def dock_bike(bike)
+        # return the bike we dock
+        # however, use instance variable to store the bike in the state of this instance
+        @bike = bike
+    end
+    # instead of...
+         #def bike
+             #@bike
+        #end
+    # we can use attr_reader to read the @bike attribute
+    attr_reader :bike
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,19 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'docks something' do
+    bike = Bike.new
+    # Dock the bike at a docking station
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+
+  # bike method to call on the status of the bike
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    # return the bike we just docked
+    expect(subject.bike).to eq bike
+  end
+
 end
