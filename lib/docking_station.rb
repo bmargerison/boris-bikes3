a61e071530c31f20456bd43f54a6ce5a542ce7fe
Dylan James <djame011@gold.ac.uk>
2021-10-21 16:51:34 +0100

    Chapter 14 amendments

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,21 +3,28 @@ require_relative 'bike'
 class DockingStation
 
     # we can use attr_reader to read the @bike attribute
-    attr_reader :bike
+    attr_reader :bikes
+    attr_reader :bike_dock
+
+    def initialize
+        @bike_dock = []
+    end
 
     def docking_station
     end
 
     def release_bike
-       fail 'No bikes available' unless @bike
-       @bike
+       fail 'No bikes available' unless @bike_dock.any?
+       @bike_dock.pop
     end
 
     def dock_bike(bike)
         # return the bike we dock
         # however, use instance variable to store the bike in the state of this instance
-        fail 'Station is full' if @bike
-        @bike = bike
+        fail 'Docking station full' if @bike_dock.count >= 20
+        @bike_dock.push(bike)
+        bike
     end
 
 end
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,25 +13,26 @@ describe DockingStation do
   it 'docks something' do
     bike = Bike.new
     # Dock the bike at a docking station
-    expect(subject.dock_bike(bike)).to eq bike
+    docking_station = DockingStation.new
+    expect { docking_station.dock_bike(bike) }.to change{docking_station.bike_dock}.from([]).to([bike])
   end
 
   # bike method to call on the status of the bike
   it 'returns docked bikes' do
     bike = Bike.new
+    docking_station = DockingStation.new
     subject.dock_bike(bike)
     # return the bike we just docked
-    expect(subject.bike).to eq bike
+    expect(docking_station.dock_bike(bike)).to eq bike 
   end
 
   it 'not to release bike' do
     expect{subject.release_bike}.to raise_error 'No bikes available'
   end
 
-  it 'not to dock bike when at full capacity' do
-    bike = Bike.new
-    subject.dock_bike(bike)
-    expect{subject.dock_bike(bike)}.to raise_error 'Station is full'
+  it 'raises an error when full' do
+    20.times { subject.dock_bike Bike.new }
+    expect { subject.dock_bike Bike.new }.to raise_error 'Docking station full'
   end
 
 end
